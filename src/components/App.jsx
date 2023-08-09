73aa0429b5597305a9a34f1df6df657f4b91f35c
mariana-nova <119463538+mariana-nova@users.noreply.github.com>
2023-08-09 16:38:27 -0500

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
+import React, { useState } from 'react';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+import '../index.css';
+
+const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (option) => {
+    setState((prevState) => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Section title="Please leave your feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {totalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
 };
+
+export default App;
